d43cd6df979762e5ef70758ec70cdd026638660c
Robert Tang <robert.tang@live.com>
2021-05-08 12:30:44 -0400

    first init

--- backend/placeholder.py
+++ backend/placeholder.py
@@ -1,2 +1,17 @@
-print("this is a placeholder")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class UserInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    # answers_first_qs = ???
+
+    def __repr__(self):
+        return '<User %r>' % self.id
